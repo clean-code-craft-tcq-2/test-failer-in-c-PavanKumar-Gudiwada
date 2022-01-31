1944b616d5d7dec44a86373cc1bda79284a81e38
PavanKumar-Gudiwada <91024463+PavanKumar-Gudiwada@users.noreply.github.com>
2022-01-31 18:22:37 +0530

    Update tshirts for better ranges of shirt sizes

--- tshirts.c
+++ tshirts.c
@@ -1,25 +1,36 @@
 #include <stdio.h>
+#include <string.h>
 #include <assert.h>
 
-char size(int cms) {
-    char sizeName = '\0';
-    if(cms < 38) {
-        sizeName = 'S';
-    } else if(cms > 38 && cms < 42) {
-        sizeName = 'M';
-    } else if(cms > 42) {
-        sizeName = 'L';
-    }
+char* size(int cms) {
+    char sizeName[3] = "\0";
+    if((cms >= 36)&&(cms < 38)) {
+        strcpy(sizeName, "S");
+    } else if(cms >= 38 && cms < 40) {
+        strcpy(sizeName, "M");
+    } else if(cms >= 40 && cms < 42) {
+        strcpy(sizeName, "L");
+    } else if(cms >= 42 && cms < 44){
+    	strcpy(sizeName, "XL");
+	} else if(cms >= 44 && cms <= 46){
+    	strcpy(sizeName, "XXL");
+	} else if((cms < 36)||(cms > 46))
+    {
+    	strcpy(sizeName, "NA");
+	}
     return sizeName;
 }
 
 int main() {
-    assert(size(37) == 'S');
-    assert(size(40) == 'M');
-    assert(size(43) == 'L');
-    assert(size(38) == 'M');
-    assert(size(-38) == '0');
-    assert(size(100) == '0');
+    assert(strcmp(size(37), "S") == 0);
+    assert(strcmp(size(38), "M") == 0);
+    assert(strcmp(size(40), "L") == 0);
+    assert(strcmp(size(43), "XL") == 0);
+    assert(strcmp(size(46), "XXL") == 0);
+    assert(strcmp(size(-38), "NA") == 0);
+    assert(strcmp(size(10), "NA") == 0);
+    assert(strcmp(size(100), "NA") == 0);
+    
     printf("All is well (maybe!)\n");
     return 0;
 }
